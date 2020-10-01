4847296939fb01a12285448180acb5d153a8cf46
Marifatul <arfiansidq@gmail.com>
2020-10-01 15:45:42 +0700

    auth

--- database/migrations/2020_08_27_081842_create_authors_table.php
+++ database/migrations/2020_08_27_081842_create_authors_table.php
@@ -20,7 +20,7 @@ class CreateAuthorsTable extends Migration
             $table->string('place_of_birth');
             $table->string('gender');
             $table->string('email');
-            $table->varchar(20)('hp');
+            $table->string('hp');
             $table->timestamps(); //otomatis akan membuat dua colom create_at dan update_at
         });
     }
